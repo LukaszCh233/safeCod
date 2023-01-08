7c4975341963bb7b3372218619573c3a1db7e0cf
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2023-01-08 13:19:54 +0100

    Add files via upload

--- FindCode.java
+++ FindCode.java
@@ -9,30 +9,25 @@ public class FindCode {
         System.out.println("CODE HAVE 5 DIGIT");
         Random rcod = new Random();
         int find = rcod.nextInt(89999) + 10000;
-
-        System.out.println("*****");
+        System.out.println(find);
         String myNumber;
         String l = find+ "";
+
 
         do {
             trials++;
             System.out.println("GIVE A NUMBER:");
             myNumber = scanner.nextLine();
-            if (myNumber.substring(0,1).equals(l.substring(0,1))) {
-                System.out.println("the first number is correct");
+            int iterator = 0;
+            char[] player = myNumber.toCharArray();
+            for (char gueesNumber : l.toCharArray())
+            {
+                if (player[iterator] == gueesNumber) {
+                    System.out.println("Number of position "+( iterator+ 1) +" is correcct");
+                }
+                iterator++;
             }
-            if (myNumber.substring(1,2).equals(l.substring(1,2))) {
-                System.out.println("the second number is correct");
-            }
-            if (myNumber.substring(2,3).equals(l.substring(2,3))) {
-                System.out.println("the third number is correct");
-            }
-            if (myNumber.substring(3,4).equals(l.substring(3,4))) {
-                System.out.println("the fourth number is correct");
-            }
-            if (myNumber.substring(4,5).equals(l.substring(4,5))) {
-                System.out.println("the fifth number is correct");
-            }
+
         } while (!myNumber.equals(l));
         System.out.println("GOOD YOU WIN");
         System.out.println("NUMBER OF ATTEMTS: "+ trials);
